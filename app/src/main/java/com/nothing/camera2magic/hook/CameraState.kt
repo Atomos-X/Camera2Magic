0c99e04e9377fabbdb75ed39d7824fbf224386d0
Atomos <kouryu.lyu@gmail.com>
2026-02-12 17:25:35 +0800

    pass previewSize to native

--- app/src/main/java/com/nothing/camera2magic/hook/CameraState.kt
+++ app/src/main/java/com/nothing/camera2magic/hook/CameraState.kt
@@ -1,22 +1,15 @@
 package com.nothing.camera2magic.hook
 import android.view.Surface
-/**
- * @param apiLevel 1: Camera1, 2: Camera2 x: CameraX
- * @param sensorOrientation 90 / 270
- * @param pictureWidth not the final picture size
- * @param pictureHeight not the final picture size
- * @param packageName app package name
- * @param displayOrientation 0, 90, 180, 270
- * @param surface preview surface
- */
 
 data class CameraState(
+    var packageName: String = "",
     var apiLevel: Int = 0,
     var facingFront: Boolean = false,
+    var pictureWidth: Int = 0,
+    var pictureHeight: Int = 0,
+    var previewWidth: Int = 0,
+    var previewHeight: Int = 0,
     var sensorOrientation: Int = 90,
-    var pictureWidth: Int = 1920,
-    var pictureHeight: Int = 1080,
-    var packageName: String = "",
     var displayOrientation: Int = 0,
-    var surface: Surface? = null
+    var surface: Surface? = null,
 )
