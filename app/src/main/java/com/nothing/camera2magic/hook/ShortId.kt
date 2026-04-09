ba203b75adfd3307ee584ef986e515e4e3f96c03
Atomos <kouryu.lyu@gmail.com>
2026-04-09 08:03:23 +0800

    Remove Legacy api support.

--- app/src/main/java/com/nothing/camera2magic/hook/ShortId.kt
+++
@@ -1,5 +0,0 @@
-package com.nothing.camera2magic.hook
-
-val Any?.shortId : String
-    get() = if (this == null) "null"
-    else "${this::class.java.simpleName}@0x${Integer.toHexString(System.identityHashCode(this))}"
